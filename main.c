138217ede499f60978cb43f0797452c6567b54f7
Stephen P <123outerme@outlook.com>
2018-03-09 23:22:45 -0500

    Manual push since git is being annoying

--- main.c
+++ main.c
@@ -2,5 +2,8 @@
 
 int main(int argc, char* argv[])
 {
-    return 0;
+    int launchCode = initSDL("C-Speed", ".", FONT_FILE_NAME, 32 * 20, 32 * 15, 24);
+    drawText("This is a test!", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, (SDL_Color) {0, 0, 0}, true);
+    waitForKey();
+    return launchCode;
 }
