76851dd3b0991b2a4e171a714ef1a56247955ac1
Stephen Policelli <123outerme@outlook.com>
2018-03-09 23:49:28 -0500

    Actually did more coding

--- main.c
+++ main.c
@@ -2,8 +2,13 @@
 
 int main(int argc, char* argv[])
 {
-    int launchCode = initSDL("C-Speed", ".", FONT_FILE_NAME, 32 * 20, 32 * 15, 24);
-    drawText("This is a test!", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, (SDL_Color) {0, 0, 0}, true);
+    int launchCode = argc;
+    if (argc > 0 && argv[0])
+        launchCode = 0;
+    launchCode = initSDL("C-Speed", ".", FONT_FILE_NAME, 32 * 20, 32 * 15, 24);
+    SDL_SetRenderDrawColor(mainRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+    SDL_RenderClear(mainRenderer);
+    drawText("This is a test!", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, (SDL_Color) {0, 0, 0, 0xFF}, true);
     waitForKey();
     return launchCode;
 }
